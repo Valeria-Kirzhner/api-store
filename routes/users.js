707821d8f3fe8edebd2450f20de7ea7417e15c80
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2022-12-15 19:29:52 +0200

    password encryption added and moved into helper file.

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,30 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
 const router = express.Router();
 const _ = require("lodash");
 const mysqlConnection = require("../utils/database");
+const helper = require("../utils/userHelper");
 
 router.post("/", async (req, res) => {
   let bodyRequest = req.body;
   const sql =
-    "INSERT INTO users (first_name,last_name,email,password, premium) VALUES(?,?,?,?,?);";
+    "INSERT INTO users (first_name,last_name,email,password,premium) VALUES(?,?,?,?,?);";
+
+  const hashedPassword = await helper.encryptPassword(bodyRequest.password);
+
   const values = [
     bodyRequest.first_name,
     bodyRequest.last_name,
     bodyRequest.email,
-    bodyRequest.password,
+    hashedPassword,
     bodyRequest.premium,
   ];
-  //   const salt = await bcrypt.genSalt(10);
-  //   hashedPassword = await bcrypt.hash(bodyRequest.Password, salt);
 
   mysqlConnection.query(sql, values, (err, result, fields) => {
     if (!err) {
-      res.status(200).send(result.affectedRows);
+      return res.sendStatus(200);
     } else {
       console.log(err);
-      return res.status(400).send("User is not registered.");
+      return res.sendStatus(400).render("User is not registered.");
     }
   });
 });
