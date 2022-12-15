f8d9a9fb3a6da6f08e976d176fd7da80c531716b
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2022-12-15 13:04:41 +0200

    home and footer have added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <header></header>
+      <main>
+        <Home />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <p className="border-top pt-3 text-center">
+      Api Store © {new Date().getFullYear()} - by Valeria Kirzhner
+    </p>
+  );
+};
+
+export default Footer;
